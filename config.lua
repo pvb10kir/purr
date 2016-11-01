1971a6f94cc34c8232b8a7d3a9a02dbe7cbca0b1
3pehrdev <sepehrsalehi0111@gmail.com>
2016-11-01 20:28:55 +0330

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,9 +1,9 @@
-bot_token = "245451534:AAEgrx-zGY_wRWkR0qYHi2xjSNL-qEjMZFU"
+bot_token = "270366540:AAHJtCnhGcROh-Z9gLzJiE_Bo8KkPIR-odQ"
 send_api = "https://api.telegram.org/bot"..bot_token
-bot_version = "6.0"
+bot_version = "1.0"
 sudo_name = "Engineer Shayan Ahmadi"
-sudo_id = 259300954
-admingp = -135847900
-sudo_num = "989351372038"
-sudo_user = "shayan_soft"
-sudo_ch = "UmbrellaTeam"
+sudo_id = 255317894
+admingp = -154002345
+sudo_num = "989309649221"
+sudo_user = "MrBlackLife"
+sudo_ch = "Sphero_Ch"
